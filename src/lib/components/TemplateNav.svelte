6c83b9f8cd00cfd72ec9a6b256c6050647e9ffc0
Russell Warner <rewarner@russellbits.com>
2023-09-19 19:00:28 -0400

    Added markdown feature to the template navigatiob component.

--- src/lib/components/TemplateNav.svelte
+++ src/lib/components/TemplateNav.svelte
@@ -1,10 +1,20 @@
-<script></script>
+<script>
+	import SvelteMarkdown from 'svelte-markdown';
+
+	const source = `
+	## Fiction Machine 
+	A layout that I largely use for publishing my fiction online. I think it makes
+	for good reading.
+	## Magazine 
+	A magazine-style layout with large sans-serif and slab fonts loosely
+	based on the style of [Teehan + Lax](https://teehanlax.com/)
+	## Scriptdown 
+	This template will
+	actually convert markdown into a screenplay style format.`;
+</script>
 
 <sidebar>
-	## Fiction Machine A layout that I largely use for publishing my fiction online. I think it makes
-	for good reading. ## Magazine A magazine-style layout with large sans-serif and slab fonts loosely
-	based on the style of [Teehan + Lax](https://teehanlax.com/) ## Scriptdown This template will
-	actually convert markdown into a screenplay style format.
+	<SvelteMarkdown {source} />
 </sidebar>
 
 <style>
