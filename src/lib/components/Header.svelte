37beff419c31f6ef8e391d77026521316b2e22e4
Russell Warner <rewarner@russellbits.com>
2025-04-01 16:00:13 -0400

    Simplified Header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header>
-	<h1><a href="/">IA Template Development Page</a></h1>
+	<h1><a href="https://www.ia-writer.com">IA Writer Template</a> Catalog</h1>
 	<p>from<a href="http//:russellbits.com">Russellbits</a></p>
 </header>
 
@@ -8,12 +8,17 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		color: white;
+		background-color: black;
+		padding: 1rem;
 	}
 	header h1 {
+		font-family: 'IBM Plex Serif';
 		padding: 0;
 		margin: 0;
 	}
 	header a {
 		color: white;
+		text-decoration: underline;
 	}
 </style>
