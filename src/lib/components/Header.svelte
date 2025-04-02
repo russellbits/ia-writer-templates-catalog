f21bc150e5ed579570667fc118a65a67ba2d91d4
Russell Warner <rewarner@russellbits.com>
2025-04-01 21:13:17 -0400

    Stylistic changes to app header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,24 +1,34 @@
 <header>
 	<h1><a href="https://www.ia-writer.com">IA Writer Template</a> Catalog</h1>
-	<p>from<a href="http//:russellbits.com">Russellbits</a></p>
+	<p>Brought to you by <a href="https://russellbits.com">Russellbits</a></p>
+	<div id="creditlogo"><img src="/images/credit-russellbits.png" alt="Credit: Russellbits" /></div>
 </header>
 
 <style>
 	header {
+		position: relative;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		color: white;
-		background-color: black;
+		background-color: #5a70e2;
 		padding: 1rem;
+		margin: 0;
 	}
 	header h1 {
 		font-family: 'IBM Plex Serif';
+		font-weight: 200;
+		font-style: normal;
 		padding: 0;
 		margin: 0;
 	}
 	header a {
 		color: white;
 		text-decoration: underline;
+	}
+	#creditlogo {
+		position: absolute;
+		bottom: -5px;
+		right: 0;
 	}
 </style>
